7c0ebc980877d20eb7da543b4fd244b3c24d8cff
Tristan Ramilo <tristanxanderjohn@hotmail.com>
2021-03-02 21:24:55 +0000

    Added Users controller, routes and show + removed create/new method for user_games

--- app/controllers/user_games_controller.rb
+++ app/controllers/user_games_controller.rb
@@ -1,20 +1,2 @@
 class UserGamesController < ApplicationController
-  def new
-    @user_game = UserGame.new
-    @game = Game.find(params[:game_id])
-  end
-
-  def create
-    @game = Game.find(params[:game_id])
-    @user_game = Usergame.new(user_game_params)
-    @user_game.game = @game
-    @user_game.user = current_user
-    @user_game.save
-  end
-
-  private
-
-  def user_game_params
-    params.require(:user_game).permit(:game_id, :user_id)
-  end
 end
