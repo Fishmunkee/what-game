e4be4970e06718066da1fe4a1ab44f76c6417531
Tristan Ramilo <tristanxanderjohn@hotmail.com>
2021-03-04 19:37:27 +0000

    Refactored controller and show for user_games

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user_game = UserGame.where(user_id: current_user.id)
-    @user_game_owned = UserGame.where(user_id: current_user.id).where(owned: true)
-    @user_game_wishlist = UserGame.where(user_id: current_user.id).where(wishlist: true)
-    @user_game_completed = UserGame.where(user_id: current_user.id).where(completed: true)
+    @user = current_user
   end 
 end
