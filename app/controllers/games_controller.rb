88a5b5e63b170aeba43141f8ec8f076c28debed1
Oliver Norgate <olli.norgate@hotmail.co.uk>
2021-03-04 19:13:44 +0000

    stuff

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,6 +12,18 @@ class GamesController < ApplicationController
     @game = Game.new(game_params)
   end
 
+  def show
+    @game = Game.find(params[:id])
+    # @recommendations = Game.where(genre: @game.genre)
+    recommendation(@game)
+  end
+
+  def recommendation(game)
+    @game = Game.where_exists(:genres, name: game.genres.pluck(:name))
+    @games = Game.where_not_exists(:user_games, user_id: current_user.id, completed: true)
+    raise
+  end
+
   private
 
   def game_params
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
   has_many :reviews
+  # has_many :user_games
   has_and_belongs_to_many :genres
   has_and_belongs_to_many :platforms
   has_and_belongs_to_many :medium
