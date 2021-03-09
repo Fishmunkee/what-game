c4ffdc8d55b476a38a2aeea808033d56f455a6cc
Annette Wong <nettecodes@gmail.com>
2021-03-09 21:18:16 +0000

    working on recommendations

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,14 +2,13 @@ class GamesController < ApplicationController
 
   def index
     @games = Game.all
-    query = params[:q]
-    @games_result = Game.where("name ILIKE ?", "%#{query}%")
   end
 
   def show
     game_id = params[:id]
     @game = Game.find(game_id)
     @reviews = Reviews.where("game_id = ?", game_id)
+    @recommendations = recommendation(@game)
   end
 
   def search
@@ -21,18 +20,13 @@ class GamesController < ApplicationController
     @game = Game.find(rand(1..Game.count))
   end
 
-  def show
-    @game = Game.find(params[:id])
-    @recommendations = recommendation(@game)
-  end
-
   private
 
   def recommendation(game)
     @games = Game.where_exists(:genres, id: game.genres.ids)
     @games = Game.where_exists(:platforms, id: game.platforms.ids)
     @games = @games.where_not_exists(:user_games, user_id: current_user.id, completed: true)
-    @games = @games.where_not_exists(:user_games, user_id: current_user.id, recommend: true)
+    @games = @games.where_not_exists(:user_games, user_id: current_user.id, recommend: false)
   end
 
   # private
