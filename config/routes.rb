c37172bebd389f8641018fc92682760dd21b2644
Tristan Ramilo <tristanxanderjohn@hotmail.com>
2021-03-13 14:54:07 +0000

    Added completed, owned, wishlist buttons on games show page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :usergames, only: [:index] do
+  resources :user_games, only: [:index] do
     collection do
       post 'setstatus'
     end
