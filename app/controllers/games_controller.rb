112bb4519cb93f2da4579a8331210fb28f630c6b
Annette Wong <nettecodes@gmail.com>
2021-03-24 13:15:45 +0000

    revert search

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -25,19 +25,20 @@ before_action :user
     genre_ids = params.dig(:games, :genre_ids)
     platform_ids = params.dig(:games, :platform_ids)
 
-    if query && query != ""
-
+    if query != nil
       @games = Game.where("title ILIKE ?", "%#{query}%")
+    else
+      @games = Game.all
+    end
 
-      if genre_ids && genre_ids.length > 1
-        g_ids = genre_ids[1..-1].map { |genre_id| genre_id.to_i }
-        @games = @games.where_exists(:genres, id: g_ids)
-      end
+    if genre_ids && genre_ids.length > 1
+      g_ids = genre_ids[1..-1].map { |genre_id| genre_id.to_i }
+      @games = @games.where_exists(:genres, id: g_ids)
+    end
 
-      if platform_ids && platform_ids.length > 1
-        p_ids = platform_ids[1..-1].map { |platform_id| platform_id.to_i }
-        @games = @games.where_exists(:platforms, id: p_ids)
-      end
+    if platform_ids && platform_ids.length > 1
+      p_ids = platform_ids[1..-1].map { |platform_id| platform_id.to_i }
+      @games = @games.where_exists(:platforms, id: p_ids)
     end
   end
 
